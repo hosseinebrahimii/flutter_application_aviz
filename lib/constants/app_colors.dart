bd3a500624197e9c06055c820a9208f95b04cca3
Hossein Ebrahimi <hossein.ebr4himi@gmail.com>
2023-12-17 20:46:21 +0330

    style: Add new colors to app_colors.dart

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  //Ui:
   static Color backgroundColor = const Color(0xffFFFFFF);
+  static Color buttonColor = const Color(0xffE60023);
+
+  //Texts:
+  static Color maintitleColor = const Color(0xFF101828);
   static Color subtitleColor = const Color(0xff98A2B3);
 }
