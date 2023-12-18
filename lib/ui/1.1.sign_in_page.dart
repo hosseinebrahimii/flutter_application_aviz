b9f8ce2b415dcd706e0ebda0645403cdb1b2f628
Hossein Ebrahimi <hossein.ebr4himi@gmail.com>
2023-12-18 19:14:11 +0330

    feat: Add signin_page.dart to ui

---
+++ lib/ui/1.1.sign_in_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_aviz/constants/app_colors.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: SafeArea(
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 57,
+                  width: double.infinity,
+                ),
+                title(context),
+                const SizedBox(
+                  height: 32,
+                ),
+                phoneNumberTextField(context),
+                const Spacer(),
+                nextStepButton(),
+                const SizedBox(
+                  height: 16,
+                ),
+                signUpText(context),
+                const SizedBox(
+                  height: 16,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget title(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Text(
+              'ورود به',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            Image.asset('assets/images/aviz.png')
+          ],
+        ),
+        const SizedBox(
+          height: 17,
+        ),
+        Text(
+          'خوشحالیم که مجددا آویز رو برای آگهی انتخاب کردی!',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+
+  Widget phoneNumberTextField(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4),
+      child: const TextField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          //other decoration options were tuned in theme in them main.dart file
+          hintText: 'شماره موبایل',
+        ),
+      ),
+    );
+  }
+
+  Widget nextStepButton() {
+    return SizedBox(
+      height: 40,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('مرحله بعد'),
+            Icon(Icons.chevron_right),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget signUpText(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'تا حالا ثبت نام نکردی؟',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          Text(
+            'ثبت نام',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ],
+      ),
+    );
+  }
+}
