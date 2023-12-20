7c750b75132ec887ab88b3a9ff66c36fc0adef11
Hossein Ebrahimi <hossein.ebr4himi@gmail.com>
2023-12-20 16:46:34 +0330

    feat: Add sign_up_verify_page to ui

---
+++ lib/ui/1.1.sign_up_verify_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_aviz/constants/app_colors.dart';
+import 'package:flutter_application_aviz/widgets/resend_code_verifier.dart';
+import 'package:flutter_application_aviz/widgets/verify_field_widget.dart';
+
+class SignUpVerifyPage extends StatelessWidget {
+  const SignUpVerifyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: AppColors.backgroundColor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 56,
+                  width: double.infinity,
+                ),
+                //verifying description:
+                title(context),
+                const SizedBox(
+                  height: 32,
+                ),
+                //TextFormField for placing 4 digit code:
+                VerifyFieldWidget(),
+                const SizedBox(
+                  height: 35,
+                ),
+                //45 seconds count down with a resend code button:
+                ResendCodeVerifier(
+                  timer: 45,
+                  onTap: () {},
+                ),
+                const Spacer(),
+                //a button for applying and verifying the 4 digit code:
+                applyButton(),
+                const SizedBox(
+                  height: 32,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget title(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'تایید شماره موبایل',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
+          'کد ثبت‌نام پیامک شده را وارد کنید',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+
+  Widget applyButton() {
+    return SizedBox(
+      height: 40,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('تایید ثبت ‌نام'),
+      ),
+    );
+  }
+}
